b4abc30e8d69ed0fde80ffdee8e6aeaf677ad8a2
Bony5690 <jerrybony@yahoo.com>
2016-11-11 07:00:41 -0600

    Loops

---
+++ loop.js
@@ -0,0 +1,22 @@
+for(var count = -10; count <= 19; count++) {
+  console.log(count);
+}
+
+for(var count = 10; count <= 40; count+=2){
+  if(count % 2 === 0){
+    console.log(count);
+  }
+
+}
+
+for(var count = 300; count<= 333; count+=1){
+  if(count % 2 !==0){
+    console.log(count);
+  }
+}
+
+for(var count = 5; count<= 50; count++){
+  if(count % 5 === 0 && count % 3 === 0){
+    console.log(count);
+  }
+}
